d55844a6145aa9d67c8af4e7274cdc4f3fda936d
thomthe <tomi2@web.de>
2014-10-15 16:13:15 +0200

    renamed functions, variables, agents, message, file... erased unused functions

--- functions.c
+++ functions.c
@@ -4,65 +4,51 @@
 #define distance(x1,y1,x2,y2) (sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)))
 
 
-int outputdata()
+int outputcellposition()
 {
     /* read agent memory */
     int id = get_id();
-    double x = get_x(), y = get_y();
-    
+    double points = get_points()
+    int type = get_type()
     /* add message to "location" board  (id, range, x, y, z) */
-    add_location_message(id, x, y);
+    add_location_message(id, points, type);
 
     return 0;  /* remain alive. 1 = death */
 }
 
 
-int inputdata()
+int movecells()
 {
     /* read agent memory */
     int id = get_id();
-    double x1 = get_x(), y1 = get_y();
-    double diameter = get_radius() * 2;
+    double points[] = get_points();
+    int type = get_type();
 
+    double points_temp[] = points;    
+	
+    //  
     /* temp vars  */
-    double p, x2, y2;
-    double core_distance;
-    double fx = 0, fy = 0;
-
+    double points2;
+    int id2,type2;
+	
     /* Loop through all messages */
     START_LOCATION_MESSAGE_LOOP
         /* NOTE: this IF condition is not really required due to filters */
         if((location_message->id != id)) 
         {
-            x2 = location_message->x;
-            y2 = location_message->y;
+            points2 = location_message->points;
+            type2 = location_message->type;
 
             core_distance = distance(x1,y1,x2,y2);
             
-            if(core_distance < diameter)
-            {
-                p = kr * diameter / core_distance;
-                
-                /* accumulate forces */
-                fx += (x1 - x2) * p; 
-                fy += (y1 - y2) * p; 
-            }
+		///
+		
+		
         }
     FINISH_LOCATION_MESSAGE_LOOP
     
     /* write forces to agent memory */
-    set_fx(fx);
-    set_fy(fy);
+    set_points(points_temp);
     
     return 0; 
-}
-
-
-int move()
-{
-    /* update position based on accumulated forces */
-    set_x( get_x() + get_fx() );
-    set_y( get_y() + get_fy() );
-    
-    return 0;
 }
