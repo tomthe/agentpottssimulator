51347de6e124a86365d11a1a1a3357bcd1ae95a5
thomthe <tomi2@web.de>
2014-10-15 17:27:23 +0200

    compiles, runs! cell-points are now stored in data-structures

--- functions.c
+++ functions.c
@@ -1,4 +1,5 @@
 #include "header.h"
+#include "cell2d4_agent_header.h"
 
 #define kr 0.1 /* Stiffness variable for repulsion */
 #define distance(x1,y1,x2,y2) (sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)))
@@ -8,12 +9,9 @@ int outputcellposition()
 {
     /* read agent memory */
     int id = get_id();
-    //double points[8];// = get_points();
-    double *points;
-    points = get_points();
     int type = get_type();
     /* add message to "location" board  (id, range, x, y, z) */
-    add_cellposition_message(id, points, type);
+    add_cellposition_message(id, POINTS, type);
 
     return 0;  /* remain alive. 1 = death */
 }
@@ -23,15 +21,16 @@ int movecells()
 {
     /* read agent memory */
     int id = get_id();
-    double *points;
-    points = get_points();
     int type = get_type();
-
-    double points_temp[] = points;    
+    
+    int changedp = 0; //or 1;2;3
+    double changedx,changedy;
+    fourpoints points2;
 	
+	
+    printf("Hallo Welt\n");printf("Hallo Welt %d", POINTS.p1.x);
     //  
     /* temp vars  */
-    double points2;
     int id2,type2;
 	
     /* Loop through all messages */
@@ -42,16 +41,13 @@ int movecells()
             points2 = cellposition_message->points;
             type2 = cellposition_message->type;
 
-            core_distance = distance(x1,y1,x2,y2);
             
-		///
-		
 		
         }
     FINISH_CELLPOSITION_MESSAGE_LOOP
     
     /* write forces to agent memory */
-    set_points(points_temp);
+    //set_points(points_temp);
     
     return 0; 
 }
