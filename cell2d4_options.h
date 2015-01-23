3c3b52b64ee85910842d00141f3d4c108eacb4b9
thomthe <tomi2@web.de>
2015-01-23 14:22:51 +0100

    also great...twoCellT5-pointy1...

--- cell2d4_options.h
+++ cell2d4_options.h
@@ -14,12 +14,12 @@ int cof_move_random_extra_probability[] = {2,2,0};
 int cof_percent_of_cells_that_should_move_in_one_ts = 80;
 
 int cof_volume_do_calc[] = {1,1,1};
-double cof_volume_target[] = {8.2,6.0,2.0};
+double cof_volume_target[] = {8.2,4.0,2.0};
 double cof_volume_alpha[] = {4.0,3.0,2.0};
 
 int cof_surface_do_calc[] = {1,1,1};
-double cof_surface_target[] = {8.0,8.1,6.0};
-double cof_surface_alpha[] = {0.4,1.01,2.0};
+double cof_surface_target[] = {5.0,20.1,6.0};
+double cof_surface_alpha[] = {0.6,0.2,2.0};
 
 int cof_convex_do_calc[] = {1,1,1};
 //double cof_convex_alpha[] = {2.0,2.0,2.0};
@@ -29,5 +29,5 @@ double cof_contact_alpha[] = {8.0,12.0,4.0};
 double cof_contact_distance[] = {0.2,0.2,0.1};
 double cof_contact_medium[] = {1.11,6.1,2.0};
 double cof_contact_depth[] = {0.25,0.25,0.2};
-double cof_contact_edge[n_cell_types][n_cell_types] = {{-4.0,0.9,2.0},{0.5,-5.5,2.0},{2.0,2.0,2.0}};
+double cof_contact_edge[n_cell_types][n_cell_types] = {{-4.0,-0.3,2.0},{-0.25,-5.5,2.0},{2.0,2.0,2.0}};
 double cof_contact_intersection[n_cell_types][n_cell_types] = {{180.0,180.0,52.0},{180.0,180.0,52.0},{52.0,52.0,52.0}};
