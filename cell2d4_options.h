b3cc0fa5706129c696e87ad23d757516d6c94f08
thomthe <tomi2@web.de>
2015-01-23 14:52:28 +0100

    TwoCellTypes6-lesspointy.avi

--- cell2d4_options.h
+++ cell2d4_options.h
@@ -18,16 +18,16 @@ double cof_volume_target[] = {8.2,4.0,2.0};
 double cof_volume_alpha[] = {4.0,3.0,2.0};
 
 int cof_surface_do_calc[] = {1,1,1};
-double cof_surface_target[] = {5.0,20.1,6.0};
-double cof_surface_alpha[] = {0.6,0.2,2.0};
+double cof_surface_target[] = {9.0,18.1,6.0};
+double cof_surface_alpha[] = {0.15,0.17,2.0};
 
 int cof_convex_do_calc[] = {1,1,1};
 //double cof_convex_alpha[] = {2.0,2.0,2.0};
 
 int cof_contact_do_calc[] = {1,1,1};
-double cof_contact_alpha[] = {8.0,12.0,4.0};
+double cof_contact_alpha[] = {12.0,14.0,4.0};
 double cof_contact_distance[] = {0.2,0.2,0.1};
 double cof_contact_medium[] = {1.11,6.1,2.0};
 double cof_contact_depth[] = {0.25,0.25,0.2};
-double cof_contact_edge[n_cell_types][n_cell_types] = {{-4.0,-0.3,2.0},{-0.25,-5.5,2.0},{2.0,2.0,2.0}};
+double cof_contact_edge[n_cell_types][n_cell_types] = {{-4.0,-3.3,2.0},{-3.25,-5.5,2.0},{2.0,2.0,2.0}};
 double cof_contact_intersection[n_cell_types][n_cell_types] = {{180.0,180.0,52.0},{180.0,180.0,52.0},{52.0,52.0,52.0}};
