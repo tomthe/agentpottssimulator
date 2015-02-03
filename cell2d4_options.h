6623329ccf58478196848648ae69a432ea65c6f5
thomthe <tomi2@web.de>
2015-02-03 12:55:19 +0100

    TwoColors_z11_roundRound_good_.avi

--- cell2d4_options.h
+++ cell2d4_options.h
@@ -8,26 +8,26 @@
 #define N_CORNERS 6
 #define n_cell_types 3
 
-double cof_move_step_size[] = {0.09,0.04,0.03,0.04};
+double cof_move_step_size[] = {0.04,0.04,0.03,0.04};
 int cof_move_random_extra_probability[] = {2,1,0};
 
-int cof_percent_of_cells_that_should_move_in_one_ts = 30;
+int cof_percent_of_cells_that_should_move_in_one_ts = 60;
 
 int cof_volume_do_calc[] = {1,1,1};
-double cof_volume_target[] = {4.5,5.0,2.0};
-double cof_volume_alpha[] = {4.0,3.0,2.0};
+double cof_volume_target[] = {6.5,6.5,2.0};
+double cof_volume_alpha[] = {1.0,0.4,2.0};
 
 int cof_surface_do_calc[] = {1,1,1};
-double cof_surface_target[] = {3.5,4.1,6.0};
-double cof_surface_alpha[] = {0.55,0.2,2.0};
+double cof_surface_target[] = {6.5,6.5,6.0};
+double cof_surface_alpha[] = {1.1,1.1,2.0};
 
 int cof_convex_do_calc[] = {1,1,1};
 //double cof_convex_alpha[] = {2.0,2.0,2.0};
 
 int cof_contact_do_calc[] = {1,1,1};
-double cof_contact_alpha[] = {12.0,14.0,4.0};
-double cof_contact_distance[] = {0.2,0.2,0.1};
+double cof_contact_alpha[] = {9.0,9.0,4.0};
+double cof_contact_distance[] = {0.4,0.4,0.1};
 double cof_contact_medium[] = {1.11,6.1,2.0};
-double cof_contact_depth[] = {0.25,0.5,0.2};
-double cof_contact_edge[n_cell_types][n_cell_types] = {{-19.0,-8.3,2.0},{-9.25,-19.5,2.0},{2.0,2.0,2.0}};
+double cof_contact_depth[] = {0.3,0.3,0.2};
+double cof_contact_edge[n_cell_types][n_cell_types] = {{-2.0,-1.0,2.0},{-1.0,-2.0,2.0},{2.0,2.0,2.0}};
 double cof_contact_intersection[n_cell_types][n_cell_types] = {{180.0,180.0,52.0},{180.0,180.0,52.0},{52.0,52.0,52.0}};
