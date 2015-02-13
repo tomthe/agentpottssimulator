acdeeab8949fa0b531c72fe56398e7ef6b07bd55
thomthe <tomi2@web.de>
2015-02-13 16:58:47 +0100

    paint_pil: added ability to paint the signal-agents as white dots

--- its/read_and_paint_potts_pil.py
+++ its/read_and_paint_potts_pil.py
@@ -47,9 +47,9 @@ def test_onefile():
 	pass
 
 def read_and_paint_one_timestep(filename):
-	cells, cell_types = read_data_from_one_timestep(filename)
+	cells, cell_types, signals_pos = read_data_from_one_timestep(filename)
 	#set_sim_space(cells)
-	paint_one_timestep(cells, cell_types,filename)
+	paint_one_timestep(cells, cell_types, signals_pos,filename)
 
 def get_color_from_cell_type(cell_type):
 	if cell_type==0:
@@ -63,7 +63,7 @@ def get_color_from_cell_type(cell_type):
 	return color
 
 
-def paint_one_timestep(cells,cell_types,filename):
+def paint_one_timestep(cells,cell_types,signals_pos,filename):
 	# create a canvas
 	# paint every cell
 	back = Image.new('RGBA', (im_width,im_height), (123,133,133,0))
@@ -82,6 +82,11 @@ def paint_one_timestep(cells,cell_types,filename):
 		#              fill=(255,255,255,127),outline=(255,255,255,255))
 		#back.paste(poly,mask=poly)
 		i+=1
+	for signal in signals_pos:
+		x,y = sim_coord_2_paint_coord(signal)
+		r = 2
+		pdraw.ellipse((x-r, y-r, x+r, y+r), fill=(211,211,255))
+
 	#save and show image
 	back.save(filename[:-3] + ".png")
 	#back.show()
@@ -95,13 +100,21 @@ def read_data_from_one_timestep(filename):
 	root = tree.getroot()
 	cells = []
 	cell_types = []
+	xlist=[]
+	ylist=[]
 	for cellxml in root.iter('corners'):
 		pointstring = cellxml.text
 		cell_points = pointstring_to_positions(pointstring)
 		cells.append(cell_points)
 	for cellxml in root.iter('type'):
 		cell_types.append(int(cellxml.text))
-	return cells, cell_types
+	for cellxml in root.iter('x'):
+		xlist.append(float(cellxml.text))
+	for cellxml in root.iter('y'):
+		ylist.append(float(cellxml.text))
+	signals_pos = zip(xlist,ylist)
+
+	return cells, cell_types, signals_pos
 
 def test_pointstring_to_positions():
 	positions = pointstring_to_positions("{{1.600000, 1.100000, 0.000000}, {1.600000, 0.100000, 0.000000}, {2.500000, 0.100000, 0.000000}, {2.386227, 1.058769, 0.000000}}")
@@ -140,7 +153,7 @@ def read_and_paint_many_timesteps(start,stop):
 	for i in xrange(start,stop):
 		try:
 			filename = str(i) + ".xml"
-			cells,cell_types = read_data_from_one_timestep(filename)
+			cells,cell_types,signals_pos = read_data_from_one_timestep(filename)
 			set_sim_space(cells)
 			read_and_paint_one_timestep(filename)
 			print "painted file: ", filename
