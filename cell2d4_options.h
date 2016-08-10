e26c85b1afa28d7ddd53170b54008126b5d70a47
thomthe <tomi2@web.de>
2016-08-10 17:15:58 +0200

    D - with type-specific attraction

--- cell2d4_options.h
+++ cell2d4_options.h
@@ -17,7 +17,7 @@
 double cof_move_step_size[] = {0.1,0.1,0.0,0.01};
 int cof_move_random_extra_probability[] = {10,10,0};
 double cof_push_alpha[] = {0.01,0.01,0.0};
-double cof_attraction_alpha[] = {0.0,0.0,0.0};
+double cof_attraction_alpha[] ={{0.05,0.01,0.0}, {0.01,0.05,0.0},{0.0,0.0,0.0}};
 
 int cof_percent_of_cells_that_should_move_in_one_ts[] = {40,40,50};
 
--- functions_helper.h
+++ functions_helper.h
@@ -750,8 +750,8 @@ double calc_H_contact_sat(double *corners, int moved_corner)
                         } else {//no contact, but close
 
                             double sqdistance_cells = sqdistance(current_xmachine_cell2d4->x,current_xmachine_cell2d4->y,cellposition_message->x,cellposition_message->y);
-                            attraction_vector[0] += (cellposition_message->x - current_xmachine_cell2d4->x) / sqdistance_cells;
-                            attraction_vector[1] += (cellposition_message->y - current_xmachine_cell2d4->y) / sqdistance_cells;
+                            attraction_vector[0] += cof_attraction_alpha[TYPE][cellposition_message->type] * (cellposition_message->x - current_xmachine_cell2d4->x) / sqdistance_cells;
+                            attraction_vector[1] += cof_attraction_alpha[TYPE][cellposition_message->type] * (cellposition_message->y - current_xmachine_cell2d4->y) / sqdistance_cells;
 
                         }
 
@@ -780,8 +780,6 @@ double calc_H_contact_sat(double *corners, int moved_corner)
     FINISH_CELLPOSITION_MESSAGE_LOOP
 
     //move cell according to attraction:
-    attraction_vector[0] *= cof_attraction_alpha[TYPE];
-    attraction_vector[1] *= cof_attraction_alpha[TYPE];
     if (attraction_vector[0] >= 0.0007) {attraction_vector[0] = 0.0007;}
     if (attraction_vector[1] >= 0.0007) {attraction_vector[1] = 0.0007;}
     move_all_corners_by_vector(corners,attraction_vector);
