c75656754f7f21a7399d285a8ed5122c351744c8
thomthe <tomi2@web.de>
2016-08-10 18:44:59 +0200

    back to more moderate options like video b-c

--- cell2d4_options.h
+++ cell2d4_options.h
@@ -16,8 +16,8 @@
 
 double cof_move_step_size[] = {0.1,0.1,0.0,0.01};
 int cof_move_random_extra_probability[] = {10,10,0};
-double cof_push_alpha[] = {0.01,0.01,0.0};
-double cof_attraction_alpha[n_cell_types][n_cell_types] ={{0.001,0.0002,0.0}, {0.0002,0.001,0.0},{0.0,0.0,0.0}};
+double cof_push_alpha[] = {0.05,0.05,0.0};
+double cof_attraction_alpha[n_cell_types][n_cell_types] ={{0.0,0.0,0.0}, {0.0,0.0,0.0},{0.0,0.0,0.0}};
 
 int cof_percent_of_cells_that_should_move_in_one_ts[] = {40,40,50};
 
@@ -35,10 +35,10 @@ int cof_straight_hull_do_calc[] = {1,1,1};
 double cof_straight_hull_alpha[] = {100.0,100.0,1};
 
 int cof_contact_do_calc[] = {1,1,1};
-double cof_contact_alpha[] = {100.0,200.0,9.0};
+double cof_contact_alpha[] = {30.0,60.0,9.0};
 double cof_contact_distance[] = {0.4,0.4,0.1}; //deprecated
 double cof_contact_medium[] = {5.0,0.0,1.0};
-double cof_contact_depth[] = {2,2,2,0.6};
+double cof_contact_depth[] = {0.9,0.9,0.9,0.6};
 double cof_contact_edge[n_cell_types][n_cell_types] = {{-25.5,-5.0,-5.0},{-5.0,-25.5,55},{0.0,0.0,0.0}};
 double cof_contact_intersection[n_cell_types][n_cell_types] = {{1.0,1.0,10.0},{1.0,1.0,10.0},{52.0,52.0,52.0}};
 
