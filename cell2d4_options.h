b36c8c67bf3081972a0d110dccd75401cb9a21ee
thomthe <tomi2@web.de>
2016-08-11 16:04:44 +0200

    107-end in video106- now it looks like cell sorting

--- cell2d4_options.h
+++ cell2d4_options.h
@@ -10,13 +10,13 @@
 
 #define cof_use_hamiltonian 1
 
-#define cof_T 1
+#define cof_T 5
 
 #define r 6.8
 
 double cof_move_step_size[] = {0.1,0.5,0.0,0.01};
 int cof_move_random_extra_probability[] = {10,10,0};
-double cof_push_alpha[] = {0.5,0.2,0.0};
+double cof_push_alpha[] = {0.2,0.2,0.0};
 double cof_attraction_alpha[n_cell_types][n_cell_types] ={{0.0,0.0,0.0}, {0.0,0.0,0.0},{0.0,0.0,0.0}};
 
 int cof_percent_of_cells_that_should_move_in_one_ts[] = {40,40,50};
@@ -43,9 +43,9 @@ double cof_contact_edge[n_cell_types][n_cell_types] = {{-14,-5.0,-5.0},{-5.0,-14
 double cof_contact_intersection[n_cell_types][n_cell_types] = {{1.0,1.0,10.0},{1.0,1.0,10.0},{52.0,52.0,52.0}};
 
 int cof_signal_release_propab[] = {10,50,-1,-1};
-double cof_signal_speed[] = {0.1,0.2,0.3};
+double cof_signal_speed[] = {0.5,0.2,0.3};
 int cof_signal_maxage[] = {100,150,3};
-double cof_cell_signal[n_cell_types][n_cell_types] = {{-10.0,0.0,0.0},{1.0,-10.0,0.0},{-1.0,2.0,0.0}};
+double cof_cell_signal[n_cell_types][n_cell_types] = {{-100.0,0.0,0.0},{1.0,-10.0,0.0},{-1.0,2.0,0.0}};
 
 //death rate for each cell-type. -1: no death; n: death after ca n timesteps
 long int cof_death_rate[] = {-1,-1,-1};
