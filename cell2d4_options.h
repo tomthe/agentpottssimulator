87ba4acb9bdabe25efb27da1872126fc2713b235
thomthe <tomi2@web.de>
2016-08-15 12:00:58 +0200

    111-more push, more contact...

--- cell2d4_options.h
+++ cell2d4_options.h
@@ -17,7 +17,7 @@
 
 double cof_move_step_size[] = {0.5,0.5,0.0,0.01};
 int cof_move_random_extra_probability[] = {10,10,0};
-double cof_push_alpha[] = {0.002,0.002,0.0};
+double cof_push_alpha[] = {0.02,0.02,0.0};
 double cof_attraction_alpha[n_cell_types][n_cell_types] ={{0.0,0.0,0.0}, {0.0,0.0,0.0},{0.0,0.0,0.0}};
 
 int cof_percent_of_cells_that_should_move_in_one_ts[] = {40,40,50};
@@ -36,17 +36,17 @@ int cof_straight_hull_do_calc[] = {1,1,1};
 double cof_straight_hull_alpha[] = {100.0,75.0,1};
 
 int cof_contact_do_calc[] = {1,1,1};
-double cof_contact_alpha[] = {20.0,20.0,9.0};
+double cof_contact_alpha[] = {40.0,40.0,9.0};
 double cof_contact_distance[] = {0.4,0.4,0.1}; //deprecated
 double cof_contact_medium[] = {2.0,2.0,1.0};
 double cof_contact_depth[] = {0.6,0.6,0.6,0.6};
 double cof_contact_edge[n_cell_types][n_cell_types] = {{-26,-5.0,-5.0},{-5.0,-26,55},{0.0,0.0,0.0}};
-double cof_contact_intersection[n_cell_types][n_cell_types] = {{85.0,55.0,10.0},{55.0,85.0,10.0},{52.0,52.0,52.0}};
+double cof_contact_intersection[n_cell_types][n_cell_types] = {{45.0,35.0,10.0},{45.0,35.0,10.0},{52.0,52.0,52.0}};
 
 int cof_signal_release_propab[] = {100,200,-1,-1};
 double cof_signal_speed[] = {0.3,0.2,0.3};
 int cof_signal_maxage[] = {200,250,3};
-double cof_cell_signal[n_cell_types][n_cell_types] = {{-200.0,0.0,0.0},{1.0,-20.0,0.0},{-1.0,2.0,0.0}};
+double cof_cell_signal[n_cell_types][n_cell_types] = {{-20.0,-1.0,0.0},{-1.0,-20.0,0.0},{-1.0,2.0,0.0}};
 
 //death rate for each cell-type. -1: no death; n: death after ca n timesteps
 long int cof_death_rate[] = {-1,-1,-1};
