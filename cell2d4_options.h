e7cb92a08268e785363e893a38df427aa665ca4a
thomthe <tomi2@web.de>
2016-08-15 12:39:56 +0200

    112 straight hull

--- cell2d4_options.h
+++ cell2d4_options.h
@@ -33,7 +33,7 @@ double cof_surface_alpha[] = {2.0,3.0,3.0};
 int cof_convex_do_calc[] = {1,1,1};
 //double cof_convex_alpha[] = {2.0,2.0,2.0};
 int cof_straight_hull_do_calc[] = {1,1,1};
-double cof_straight_hull_alpha[] = {100.0,75.0,1};
+double cof_straight_hull_alpha[] = {30.0,75.0,1};
 
 int cof_contact_do_calc[] = {1,1,1};
 double cof_contact_alpha[] = {40.0,40.0,9.0};
