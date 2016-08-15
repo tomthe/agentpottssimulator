b6804249b53aaf5b836c1bd0e8caf071f930cd56
thomthe <tomi2@web.de>
2016-08-15 17:02:11 +0200

    115 - without attraction. T lowered at t=364000

--- cell2d4_options.h
+++ cell2d4_options.h
@@ -11,14 +11,14 @@
 
 #define cof_use_hamiltonian 1
 
-#define cof_T 3
+#define cof_T 1
 
 #define r 6.8
 
 double cof_move_step_size[] = {0.5,0.5,0.0,0.01};
 int cof_move_random_extra_probability[] = {10,10,0};
 double cof_push_alpha[] = {0.02,0.02,0.0};
-double cof_attraction_alpha[n_cell_types][n_cell_types] ={{0.00001,0.0,0.0}, {0.00001,0.0,0.0},{0.0,0.0,0.0}};
+double cof_attraction_alpha[n_cell_types][n_cell_types] ={{0.0,0.0,0.0}, {0.00,0.0,0.0},{0.0,0.0,0.0}};
 
 int cof_percent_of_cells_that_should_move_in_one_ts[] = {40,40,50};
 
@@ -41,7 +41,7 @@ double cof_contact_distance[] = {0.4,0.4,0.1}; //deprecated
 double cof_contact_medium[] = {2.0,2.0,1.0};
 double cof_contact_depth[] = {0.6,0.6,0.6,0.6};
 double cof_contact_edge[n_cell_types][n_cell_types] = {{-26,-5.0,-5.0},{-5.0,-26,55},{0.0,0.0,0.0}};
-double cof_contact_intersection[n_cell_types][n_cell_types] = {{45.0,35.0,10.0},{45.0,35.0,10.0},{52.0,52.0,52.0}};
+double cof_contact_intersection[n_cell_types][n_cell_types] = {{25.0,35.0,10.0},{45.0,35.0,10.0},{52.0,52.0,52.0}};
 
 int cof_signal_release_propab[] = {100,200,-1,-1};
 double cof_signal_speed[] = {0.3,0.2,0.3};
