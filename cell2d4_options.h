478c55d6c0c8e5096b0ae81e45e784d0c62432e9
thomthe <tomi2@web.de>
2016-08-23 11:17:35 +0200

    129 - 12 corners - angiogenesis - mit (shcwachen, schnellen) signals - elongated

--- cell2d4_options.h
+++ cell2d4_options.h
@@ -43,10 +43,10 @@ double cof_contact_depth[] = {0.5,0.4,0.6,0.6};
 double cof_contact_edge[n_cell_types][n_cell_types] = {{-16,-16.0,-5.0},{-16.0,-16,55},{0.0,0.0,0.0}};
 double cof_contact_intersection[n_cell_types][n_cell_types] = {{2.0,2.0,10.0},{1.0,1.0,10.0},{52.0,52.0,52.0}};
 
-int cof_signal_release_propab[] = {-1,-1,-1,-1};
-double cof_signal_speed[] = {0.05,0.1,0.3};
+int cof_signal_release_propab[] = {30,30,-1,-1};
+double cof_signal_speed[] = {0.2,0.4,0.3};
 int cof_signal_maxage[] = {100,100,3};
-double cof_cell_signal[n_cell_types][n_cell_types] = {{-10.0,-10.0,0.0},{-10.0,-10.0,0.0},{-1.0,2.0,0.0}};
+double cof_cell_signal[n_cell_types][n_cell_types] = {{-2.0,-2.0,0.0},{-2.0,-2.0,0.0},{-1.0,2.0,0.0}};
 
 //death rate for each cell-type. -1: no death; n: death after ca n timesteps
 long int cof_death_rate[] = {-1,-1,-1};
