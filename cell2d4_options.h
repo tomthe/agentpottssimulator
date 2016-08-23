22df2c0cf202e38de80b11d4e94c42bd69e69d7a
thomthe <tomi2@web.de>
2016-08-23 11:26:16 +0200

    130 - angiogenesis with asymetric cell division

--- cell2d4_options.h
+++ cell2d4_options.h
@@ -15,9 +15,9 @@
 
 #define r 6.8
 
-double cof_move_step_size[] = {0.09,0.09,0.0,0.01};
+double cof_move_step_size[] = {0.12,0.01,0.0,0.01};
 int cof_move_random_extra_probability[] = {10,10,0};
-double cof_push_alpha[] = {0.005,0.005,0.0};
+double cof_push_alpha[] = {0.1,0.005,0.0};
 double cof_attraction_alpha[n_cell_types][n_cell_types] ={{0.0,0.0,0.0}, {0.00,0.0,0.0},{0.0,0.0,0.0}};
 
 int cof_percent_of_cells_that_should_move_in_one_ts[] = {40,40,50};
@@ -27,7 +27,7 @@ double cof_volume_target[] = {5.0,4.0,0.5};
 double cof_volume_alpha[] = {10.0,10.0,1.0};
 
 int cof_surface_do_calc[] = {1,1,0};
-double cof_surface_target[] = {15.0,17.0,6.0};
+double cof_surface_target[] = {9.0,9.0,6.0};
 double cof_surface_alpha[] = {2.0,1.0,3.0};
 
 int cof_convex_do_calc[] = {0,1,1};
@@ -43,11 +43,11 @@ double cof_contact_depth[] = {0.5,0.4,0.6,0.6};
 double cof_contact_edge[n_cell_types][n_cell_types] = {{-16,-16.0,-5.0},{-16.0,-16,55},{0.0,0.0,0.0}};
 double cof_contact_intersection[n_cell_types][n_cell_types] = {{2.0,2.0,10.0},{1.0,1.0,10.0},{52.0,52.0,52.0}};
 
-int cof_signal_release_propab[] = {30,30,-1,-1};
+int cof_signal_release_propab[] = {-1,-1,-1,-1};
 double cof_signal_speed[] = {0.2,0.4,0.3};
 int cof_signal_maxage[] = {100,100,3};
 double cof_cell_signal[n_cell_types][n_cell_types] = {{-2.0,-2.0,0.0},{-2.0,-2.0,0.0},{-1.0,2.0,0.0}};
 
 //death rate for each cell-type. -1: no death; n: death after ca n timesteps
 long int cof_death_rate[] = {-1,-1,-1};
-long int cof_divide_rate[] = {-1,-1,-1};
+long int cof_divide_rate[] = {300,-1,-1};
