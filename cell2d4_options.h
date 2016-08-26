203b769cdc2ce02f12ffb483309166275e76e87d
thomthe <tomi2@web.de>
2016-08-26 10:30:48 +0200

    139 - angio...proliferation

--- cell2d4_options.h
+++ cell2d4_options.h
@@ -15,9 +15,9 @@
 
 #define r 6.8
 
-double cof_move_step_size[] = {0.12,0.06,0.0,0.01};
+double cof_move_step_size[] = {0.2,0.06,0.0,0.01};
 int cof_move_random_extra_probability[] = {10,10,0};
-double cof_push_alpha[] = {0.6,0.03,0.0};
+double cof_push_alpha[] = {0.8,0.03,0.0};
 double cof_attraction_alpha[n_cell_types][n_cell_types] ={{0.0,0.0,0.0}, {0.00,0.0,0.0},{0.0,0.0,0.0}};
 
 int cof_percent_of_cells_that_should_move_in_one_ts[] = {60,12,50};
@@ -43,10 +43,10 @@ double cof_contact_depth[] = {0.5,0.4,0.6,0.6};
 double cof_contact_edge[n_cell_types][n_cell_types] = {{2.0,-1.0,-5.0},{-8.0,-16,55},{0.0,0.0,0.0}};
 double cof_contact_intersection[n_cell_types][n_cell_types] = {{2.0,2.0,10.0},{1.0,1.0,10.0},{52.0,52.0,52.0}};
 
-int cof_signal_release_propab[] = {-1,-1,-1,-1};
-double cof_signal_speed[] = {0.2,0.4,0.3};
-int cof_signal_maxage[] = {100,100,3};
-double cof_cell_signal[n_cell_types][n_cell_types] = {{-2.0,-2.0,0.0},{-2.0,-2.0,0.0},{-1.0,2.0,0.0}};
+int cof_signal_release_propab[] = {50,-1,-1,-1};
+double cof_signal_speed[] = {0.4,0.4,0.3};
+int cof_signal_maxage[] = {300,100,3};
+double cof_cell_signal[n_cell_types][n_cell_types] = {{5.0,0.0,0.0},{0.0,-2.0,0.0},{-1.0,2.0,0.0}};
 
 //death rate for each cell-type. -1: no death; n: death after ca n timesteps
 long int cof_death_rate[] = {-1,-1,-1};
